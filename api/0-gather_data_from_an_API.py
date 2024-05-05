c0fff79b0f8a8ea7c1515dced7b0968c82425b9b
fuartkhana <146414387+fuartkhana@users.noreply.github.com>
2024-05-05 23:46:24 +0400

    Added Task 0

--- api/0-gather_data_from_an_API.py
+++ api/0-gather_data_from_an_API.py
@@ -1,54 +1,36 @@
 #!/usr/bin/python3
-"""
-This module retrieves task information from an API based on a user ID
-anddisplays the employee's name,
-the number of tasks completed, and the titles of the completed tasks.
-"""
-
+"""Tasks Related to API"""
 import requests
-import sys
-
-TASK_URL = "https://jsonplaceholder.typicode.com/todos"
-USER_URL = "https://jsonplaceholder.typicode.com/users/"
+from sys import argv
 
 
-def employee_info(user_id: str):
-    """
-    Retrieves task information from an API based
-    on a user ID and returns a formatted string
-    containing the employee's name, the number of tasks completed, and the
-    titles of the completed tasks.
+def main(id):
+    """Gets the info and retrives the tasks"""
+    base_url = "https://jsonplaceholder.typicode.com"
+    user_url = "{}/users/{}".format(base_url, id)
+    todo_url = "{}/todos?userId={}".format(base_url, id)
 
-    Args:
-        user_id (str): The ID of the user.
+    user = requests.get(user_url).json()
+    todos = requests.get(todo_url).json()
 
-    Returns:
-        str: A formatted string containing the
-        employee's name, the number of tasks completed,
-        and the titles of the completed tasks.
-    """
-    task_response = requests.get(TASK_URL + "?userId=" + user_id).json()
-    user_response = requests.get(USER_URL + user_id).json()
-
-    name = user_response.get("name")
-    total_tasks = len(task_response)
-    done_tasks = len([task for task in task_response
-                      if task.get("completed") is True])
-    title_tasks = [
-        task.get("title") for task in task_response
-        if task.get("completed") is True
+    user_name = user.get("name")
+    total_tasks = len(todos)
+    completed_tasks = [
+        task.get("title") for task in todos if task.get("completed")
     ]
+    completed_tasks_count = len(completed_tasks)
 
-    result = "Employee {} is done with tasks({}/{}):\n\t ".format(
-        name, done_tasks, total_tasks
+    print(
+        "Employee {} is done with tasks({}/{}):".format(
+            user_name, completed_tasks_count, total_tasks
+        )
     )
-    result += "\n\t ".join(title_tasks)
 
-    return result
+    for task in completed_tasks:
+        print("\t " + task)
 
 
 if __name__ == "__main__":
-    if len(sys.argv) != 2:
-        raise ValueError("Usage: 0-gather_data_from_an_API.py <employee_id>")
-    user_id = sys.argv[1]
-    print(employee_info(user_id))
+    if len(argv) == 2:
+        id = int(argv[1])
+        main(id)
