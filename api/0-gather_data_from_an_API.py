ffc4d89c08532346b1ef1df4b81975f5c7b50d8c
fuartkhana <146414387+fuartkhana@users.noreply.github.com>
2024-05-05 23:34:19 +0400

    Added Task 0

---
+++ api/0-gather_data_from_an_API.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+"""Tasks related to API"""
+
+
+if __name__ == '__main__':
+    import requests
+    import sys
+    userid = sys.argv[1]
+    NUMBER_OF_DONE_TASKS = 0
+    TASK_TITLE = []
+    data = requests\
+        .get(f'https://jsonplaceholder.typicode.com/users/{userid}')\
+        .json().get('name')
+    todos = requests\
+        .get(f'https://jsonplaceholder.typicode.com/users/{userid}/todos')\
+        .json()
+    TOTAL_NUMBER_OF_TASKS = len(todos)
+    for tasks in todos:
+        if tasks.get('completed'):
+            NUMBER_OF_DONE_TASKS += 1
+            TASK_TITLE.append(tasks.get('title'))
+    print(f'Employee {data} is done with tasks'
+          f'({NUMBER_OF_DONE_TASKS}/{TOTAL_NUMBER_OF_TASKS}):')
+    for title in TASK_TITLE:
+        print(f'\t {title}')
